f7d14840ba3bb345090332ea04376cb48c1ee38a
satoshi-python <3104chalo@gmail.com>
2022-06-12 16:12:22 +0900

    first

---
+++ ex86/env_django/db/init/02_insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO book VALUES(DEFAULT, "プログラミング言語Ｃ", current_timestamp);
+INSERT INTO book VALUES(DEFAULT, "優しいコンピュータ科学",current_timestamp);
+INSERT INTO book VALUES(DEFAULT, "ゲーテル、エッシャー、バッハーあるいはフシギの環",current_timestamp);
+INSERT INTO book VALUES(DEFAULT, "TeXブックコンピューターによる組板システム",current_timestamp);
+INSERT INTO book VALUES(DEFAULT, "人月の神話オオカミ人間を撃つ銀の弾はない",current_timestamp);
